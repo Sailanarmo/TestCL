cfedc3eba46de8c5cce559b6b4469e7d68f29c16
Sailanarmo <rlramirez12@gmail.com>
2020-03-24 23:35:22 -0600

    Trying this script.

---
+++ .WinSetup/configOCL.ps1
@@ -0,0 +1,13 @@
+mkdir ../OpenCL
+cd ../OpenCL
+
+mkdir lib ; mkdir include ; cd lib ; mkdir x64 ; cd .. ; cd include ; mkdir CL ; cd ../../
+
+mv TestCL/OpenCL/lib\Release/OpenCL.lib OpenCL/lib/x64
+mv TestCL/OpenCL/lib/Release/OpenCL.dll OpenCL/lib/x64
+
+mv TestCL/OpenCL/OpenCL-ICD-Loader/inc/CL/*.h OpenCL/include/CL
+
+cd OpenCL
+
+$Env:OCL_ROOT=(Get-Item -Path ".\").FullName
